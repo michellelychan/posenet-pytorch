cfee7b1098a62c53765056d5713fdc56f8e497f5
Michelle Chan <michellechan@Michelles-MacBook-Pro.local>
2023-02-24 15:16:28 -0800

    update JointMSELoss

--- train.py
+++ train.py
@@ -18,7 +18,8 @@ from torchvision import transforms
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=int, default=101)
-parser.add_argument('--image_dir', type=str, default='./images')
+parser.add_argument('--train_image_dir', type=str, default='./images_train')
+parser.add_argument('--test_image_dir', type=str, default= "./images_test")
 parser.add_argument('--output_dir', type=str, default='./output')
 args = parser.parse_args()
 
@@ -32,7 +33,7 @@ class JointsMSELoss(nn.Module):
         batch_size = output.size(0)
         num_joints = output.size(1)
         heatmap_size = output.size(2)
-        print("heatmap size: ", heatmap_size)
+        # print("heatmap size: ", heatmap_size)
         
         # heatmaps_pred = output.reshape((batch_size, num_joints, -1)).split(1, 1)
         # heatmaps_gt = target.reshape((batch_size, num_joints, -1)).split(1, 1)
@@ -100,14 +101,14 @@ class PosenetDatasetImage(Dataset):
         )
         
         
-        print(filename)
-        print(input_image.shape)
+        # print(filename)
+        # print(input_image.shape)
         
         input_image_tensor = torch.Tensor(input_image).cuda()
         #print("Tensor shape: ", input_image_tensor.shape[-2:])
         if input_image_tensor.shape[-2:] != (513, 513):
             input_image_resized = nn.functional.interpolate(input_image_tensor, size=(513, 513), mode='bilinear', align_corners=True)
-            print(f"Resized image {filename}: ", input_image_resized.shape)
+            # print(f"Resized image {filename}: ", input_image_resized.shape)
             return input_image_resized, draw_image, output_scale
         else:
             return input_image_tensor, draw_image, output_scale
@@ -123,37 +124,36 @@ def train(model, train_loader, test_loader, criterion, optimizer, num_epochs):
         
         print(train_loader)
         
+        torch.autograd.set_detect_anomaly(True)
         for batch_idx, (data, target, _) in enumerate(train_loader):
-            print("ENUMERATE")
+            # print("ENUMERATE")
             
             data.cuda()
             data_squeezed = data.squeeze()
             target.cuda()
             
-            print("Train Target type: ", type(target))
-            print("Train Target [1] shape: ", target[1].shape)
+            # print("Train Target type: ", type(target))
+            # print("Train Target [1] shape: ", target[1].shape)
             
             # data = torch.transpose(data, 1, 3)
             # data, target = data.cuda(), target.cuda()
             # Forward pass
             output = model(data_squeezed)
-            print("Output type: ", type(output))
+            # print("Output type: ", type(output))
             
             
             
-            print("finished model")
-            print("Train Output type: ", type(output[0]))
-            print("Train Output Length: ", len(output))
+            # print("finished model")
+            # print("Train Output type: ", type(output[0]))
+            # print("Train Output Length: ", len(output))
             
             #heatmap tensor = output[0] 
             #heatmap size is num of images x 17 keypoints x resolution x resolution 
             #eg. if image size is 225 with output stride of 16, then resolution is 15 
-            
-            
-            print(output[0].shape)
-            print(output[1].shape)
-            print(output[2].shape)
-            print(output[3].shape)
+            # print(output[0].shape)
+            # print(output[1].shape)
+            # print(output[2].shape)
+            # print(output[3].shape)
             
                         
             
@@ -171,18 +171,21 @@ def train(model, train_loader, test_loader, criterion, optimizer, num_epochs):
             loss.backward()
             optimizer.step()
 
-        # # Evaluate on test set
-        # model.eval()
-        # test_loss = 0
-        # with torch.no_grad():
-        #     for data, target, _ in test_loader:
-        #         data, target = torch.Tensor(data).cuda(), torch.Tensor(target).cuda()
-        #         output = model(data)
-        #         test_loss += criterion(output, target).item()
-        # test_loss /= len(test_loader.dataset)
+        # Evaluate on test set
+        model.eval()
+        test_loss = 0
+        with torch.no_grad():
+            for data, target, _ in test_loader:
+                data.cuda()
+                data_squeezed = data.squeeze()
+                target.cuda()
+                # data, target = torch.Tensor(data).cuda(), torch.Tensor(target).cuda()
+                output = model(data_squeezed)
+                test_loss += criterion(output[0], target).item()
+        test_loss /= len(test_loader.dataset)
 
-        # print('Epoch: {} \tTrain Loss: {:.6f} \tTest Loss: {:.6f}'.format(
-        #     epoch+1, loss.item(), test_loss))
+        print('Epoch: {} \tTrain Loss: {:.6f} \tTest Loss: {:.6f}'.format(
+            epoch+1, loss.item(), test_loss))
         
 def main():
 
@@ -201,19 +204,19 @@ def main():
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
     # Load data
-    train_dataset = PosenetDatasetImage(args.image_dir, train=True)
-    test_dataset = PosenetDatasetImage(args.image_dir, train=False)
+    train_dataset = PosenetDatasetImage(args.train_image_dir, train=True)
+    test_dataset = PosenetDatasetImage(args.test_image_dir, train=False)
     
     
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
     
     
-    for x in train_dataset:
-        print("SHAPE+_+ ", x[1].shape)
+    # for x in train_dataset:
+        # print("SHAPE+_+ ", x[1].shape)
         
         
-    print(next(iter(train_loader)))
+    # print(next(iter(train_loader)))
 
         
     #image = next(iter(train_dataset))

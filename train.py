2c1fdd8c0cf579f17689c57e9193ec8b9d9c6214
Michelle Chan <michellechan@Michelles-MacBook-Pro.local>
2023-02-24 14:55:23 -0800

    working JointsMSELoss function

--- train.py
+++ train.py
@@ -31,13 +31,16 @@ class JointsMSELoss(nn.Module):
     def forward(self, output, target): #removed target_weight
         batch_size = output.size(0)
         num_joints = output.size(1)
-        heatmaps_pred = output.reshape((batch_size, num_joints, -1)).split(1, 1)
-        heatmaps_gt = target.reshape((batch_size, num_joints, -1)).split(1, 1)
+        heatmap_size = output.size(2)
+        print("heatmap size: ", heatmap_size)
+        
+        # heatmaps_pred = output.reshape((batch_size, num_joints, -1)).split(1, 1)
+        # heatmaps_gt = target.reshape((batch_size, num_joints, -1)).split(1, 1)
         loss = 0
 
         for idx in range(num_joints):
-            heatmap_pred = heatmaps_pred[idx].squeeze()
-            heatmap_gt = heatmaps_gt[idx].squeeze()
+            heatmap_pred = output[:, :idx, :, :].squeeze()
+            heatmap_gt = output[:, :idx, :, :].squeeze()
             # if self.use_target_weight:
             #     loss += 0.5 * self.criterion(
             #         heatmap_pred.mul(target_weight[:, idx]),

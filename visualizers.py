464bd68bdd1bdce5fee77b2bb2ee06235600a9c2
Michelle Chan <michellechan@Michelles-MacBook-Pro.local>
2023-03-26 14:21:25 -0700

    fix issue with visualizers file

--- visualizers.py
+++ visualizers.py
@@ -35,7 +35,7 @@ def print_heatmap(heatmap):
             plt.clf()
 
 
-def draw_coordinates_to_image_file(appended_text, image_path, output_dir, output_stride, scale_factor, keypoint_scores, keypoint_coords, filename, displacements_fwd=None, displacements_bwd=None, include_displacements=False):
+def draw_coordinates_to_image_file(appended_text, image_path, output_dir, output_stride, scale_factor, pose_scores, keypoint_scores, keypoint_coords, filename, displacements_fwd=None, displacements_bwd=None, include_displacements=False):
     
     print("------- inside draw_coordinates_to_image_file")
     print("appended_text: ", appended_text)
@@ -54,71 +54,72 @@ def draw_coordinates_to_image_file(appended_text, image_path, output_dir, output
     # print(f"Current dimensions: {current_width}x{current_height}")
     
     # Clone keypoint_coords tensor to avoid in-place operation error
-    keypoint_coords = keypoint_coords.clone()
-    keypoint_coords = keypoint_coords.float()
+    keypoint_coords = keypoint_coords.copy()
+    keypoint_coords = keypoint_coords.astype(np.float32)
 
     
     # Convert keypoint_coords to a NumPy array
-    keypoint_coords = keypoint_coords.detach().cpu().numpy()
+    # keypoint_coords = keypoint_coords.detach().cpu().numpy()
     
     
     # keypoint_coords = keypoint_coords * output_scale * output_stride
     
     # Add extra dimension to keypoint_scores and keypoint_coords
     #if keypoint_scores is single pose with dimension of 1, add extra dimension
-    if (len(keypoint_scores.shape) == 1):
+    # if (len(keypoint_scores.shape) == 1):
         
         # print("DRAW coords keypoint_scores original shape: ", keypoint_scores.shape) 
         # print("DRAW coords keypoint_coords original shape: ", keypoint_coords.shape) 
-        min_pose_score = 1.0
+    min_pose_score = 1.0
         
-        num_instances = keypoint_scores.shape[0] # Get the number of instances
-        instance_scores = [min_pose_score for _ in range(num_instances)]
-        
-        # Repeat the keypoint scores and keypoint coordinates as many times as the number of instances
-        keypoint_scores = np.tile(keypoint_scores[np.newaxis, :], (num_instances, 1))
-        keypoint_coords = np.tile(keypoint_coords[np.newaxis, :, :], (num_instances, 1, 1))
+    # num_instances = keypoint_scores.shape[0] # Get the number of instances
+    # instance_scores = [min_pose_score for _ in range(num_instances)]
+    
+    keypoint_coords *= output_scale
 
-        # Create a list of instance scores with the same length as the number of keypoints
+    draw_image = posenet.draw_skel_and_kp(
+        draw_image, pose_scores, keypoint_scores, keypoint_coords,
+        min_pose_score=0.25, min_part_score=0.25)
+
+
+    # Repeat the keypoint scores and keypoint coordinates as many times as the number of instances
+    # keypoint_scores = np.tile(keypoint_scores[np.newaxis, :], (num_instances, 1))
+    # keypoint_coords = np.tile(keypoint_coords[np.newaxis, :, :], (num_instances, 1, 1))
+    
+    # Create a list of instance scores with the same length as the number of keypoints
 
     # Resize the draw_image back to the original dimensions
     draw_image = cv2.resize(draw_image, (original_width, original_height))
 
-    if include_displacements:
-        print("displacements_fwd shape: ", displacements_fwd.shape)
-        # displacements_fwd_np = displacements_fwd.cpu().numpy()
-        # displacement_shape = displacements_fwd_np[0].shape
+#     if include_displacements:
+#         print("displacements_fwd shape: ", displacements_fwd.shape)
+#         # displacements_fwd_np = displacements_fwd.cpu().numpy()
+#         # displacement_shape = displacements_fwd_np[0].shape
 
-        # Create a mesh grid for the quiver plot of the displacements
-        # Y, X = np.mgrid[0:displacement_shape[0], 0:displacement_shape[1]]
+#         # Create a mesh grid for the quiver plot of the displacements
+#         # Y, X = np.mgrid[0:displacement_shape[0], 0:displacement_shape[1]]
         
 
-        # Overlay the displacements onto the draw_image
-        # draw_image_quiver = cv2.cvtColor(draw_image, cv2.COLOR_BGR2RGB)  # Convert to RGB for matplotlib
-        # fig, ax = plt.subplots()
-        # ax.imshow(draw_image_quiver)
+#         # Overlay the displacements onto the draw_image
+#         # draw_image_quiver = cv2.cvtColor(draw_image, cv2.COLOR_BGR2RGB)  # Convert to RGB for matplotlib
+#         # fig, ax = plt.subplots()
+#         # ax.imshow(draw_image_quiver)
 
-        print("Before draw_displacement_vectors")
+#         print("Before draw_displacement_vectors")
 
-        fig, ax = draw_displacement_vectors(draw_image, keypoint_coords, displacements_fwd, output_stride, scale_factor)
+#         # fig, ax = draw_displacement_vectors(draw_image, keypoint_coords, displacements_fwd, output_stride, scale_factor)
         
-        print("After loop for keypoint pairs")  # Add this print statement
-        ax.axis('off')
-        print("Before saving the plot")
-        plt.savefig(os.path.join(output_dir, f"displacements_{appended_text}_{filename}"), dpi=100)
-        print("After saving the plot")
-        plt.close(fig)
-        print("After closing the plot")
+#         # print("After loop for keypoint pairs")  # Add this print statement
+#         # ax.axis('off')
+#         # print("Before saving the plot")
+#         # plt.savefig(os.path.join(output_dir, f"keypoints_{appended_text}_{filename}"), dpi=100)
+#         print("After saving the plot")
+#         # plt.close(fig)
+#         print("After closing the plot")
 
     # Draw the keypoints and skeleton on the draw_image
-    draw_image = posenet.draw_skel_and_kp(
-        draw_image, instance_scores, keypoint_scores, keypoint_coords,
-        min_pose_score=0.25, min_part_score=0.00001)
-    
     abs_output_dir = os.path.abspath(output_dir)
     print("Absolute output_dir: ", abs_output_dir)
-    
-    
     
     # Update image_path to include epoch number
     image_filename = f"{appended_text}_{filename}"
